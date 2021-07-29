c74bbfb0139bbde2dcd6dbaadc81aeccf06e8f96
DALDALGRAPH <72500196+korobopolly@users.noreply.github.com>
2021-07-29 09:53:48 +0900

    범용적인 코드를 사용한 좋아요 싫어요 플러그인 완성

--- ubdocument/action.php
+++ ubdocument/action.php
@@ -7,16 +7,14 @@ require_once($CFG->dirroot.'/local/ubdocument/preferencelib.php');
 
 $like       = optional_param('like', '', PARAM_RAW);
 $hate       = optional_param('hate', '', PARAM_RAW);
+$paramuserid   = optional_param('paramuserid', null, PARAM_INT);
 $paramcid   = optional_param('paramcid', null, PARAM_INT);
 $paramcmid  = optional_param('paramcmid', null, PARAM_INT);
 
 if(!empty($like) && $like == '좋아요'){
-    plusLike($paramcid, $paramcmid);
-    // redirect("http://localhost/mod/forum/view.php?id=5", '이 활동을 좋아합니다.', 2); 
+    plusLike($paramuserid,$paramcid, $paramcmid);
 } else if(!empty($hate) && $hate == '싫어요'){
-    plusHate($paramcid, $paramcmid);
-    // redirect("http://localhost/mod/forum/view.php?id=5", '이 활동을 싫어합니다.', 2); 
+    plusHate($paramuserid,$paramcid, $paramcmid);
 } else {
-    // redirect("http://localhost/mod/forum/view.php?id=5", '정상적으로 실행되지 않았습니다.', 2); 
 }
 ?>
--- ubdocument/preferencelib.php
+++ ubdocument/preferencelib.php
@@ -23,7 +23,7 @@
 
 
     // 버튼을 눌렀을때 값을 insert || update
-    function plusLike($cid, $cmid){
+    function plusLike($userid, $cid, $cmid){
         global $DB;
 
         $sql = "SELECT cm.id, m.name FROM {modules} m JOIN {course_modules} cm ON cm.module = m.id WHERE cm.course = :cid AND cm.id = :cmid ";
@@ -37,15 +37,17 @@
                 JOIN {course_modules} cm ON cm.course = c.id
                 JOIN {preference} p ON p.cmid = cm.id
                 JOIN {modules} m ON m.id = cm.module
-                WHERE c.id = :cid AND cm.id = :cmid ";
+                WHERE c.id = :cid AND cm.id = :cmid AND userid = :userid";
 
-        $datas = $DB->get_record_sql($sql, array('cid'=>$cid, 'cmid'=>$cmid));
+        $datas = $DB->get_record_sql($sql, array('cid'=>$cid, 'cmid'=>$cmid, 'userid'=>$userid));
 
         
 
         if(empty($datas)){
             // insert
             $object_like = new stdClass();
+            $object_like->userid = $userid;
+            $object_like->cid = $cid;
             $object_like->cmid = $cmid;
             $object_like->liked = 1;
             $object_like->hate = 0;
@@ -57,54 +59,74 @@
         } else {
             // update
             $object_like = new stdClass();
-            $object_like->id = $datas->preferenceid;
+            $object_like->userid = $userid;
+            $object_like->cid = $cid;
             $object_like->cmid = $cmid;
-            $object_like->liked = (int)$datas->liked + 1;
-            $object_like->hate = (int)$datas->hate;
+            $object_like->liked = 1;
+            $object_like->hate = 0;
             $object_like->chance = 1;
             
-
             $DB->update_record('preference', $object_like);
             redirect("/mod/".$usemodulename."/view.php?id=".$usemoduleid, '이 활동을 좋아합니다.', 2); 
         }
     }
 
-    function plusHate($cid, $cmid){
+    function plusHate($userid, $cid, $cmid){
         global $DB;
 
-        $sql = "SELECT * 
-                FROM mdl_course c
-                JOIN mdl_course_modules cm ON cm.course = c.id
-                JOIN mdl_preference p ON p.cmid = cm.id
-                WHERE c.id = :cid AND cm.id = :cmid ";
-
+        $sql = "SELECT cm.id, m.name FROM {modules} m JOIN {course_modules} cm ON cm.module = m.id WHERE cm.course = :cid AND cm.id = :cmid ";
         $datas = $DB->get_record_sql($sql, array('cid'=>$cid, 'cmid'=>$cmid));
+
+        $usemodulename = $datas->name;
+        $usemoduleid = $datas->id;
+
+        $sql = "SELECT c.id cid, cm.id cmid, cm.instance instanceid, p.id preferenceid, p.liked, p.hate, p.chance, m.name modulename
+                FROM {course} c
+                JOIN {course_modules} cm ON cm.course = c.id
+                JOIN {preference} p ON p.cmid = cm.id
+                JOIN {modules} m ON m.id = cm.module
+                WHERE c.id = :cid AND cm.id = :cmid AND userid = :userid";
+
+        $datas = $DB->get_record_sql($sql, array('cid'=>$cid, 'cmid'=>$cmid, 'userid'=>$userid));
 
         if(empty($datas)){
             // insert
-            $insert_sql = "INSERT INTO mdl_preference (`like`, hate, chance) VALUES ('0', '0', '0')";
-            $DB->insert_record($datas, $insert_sql);
+            $object_like = new stdClass();
+            $object_like->userid = $userid;
+            $object_like->cid = $cid;
+            $object_like->cmid = $cmid;
+            $object_like->liked = 0;
+            $object_like->hate = 1;
+            $object_like->chance = 1;
+
+            $DB->insert_record('preference', $object_like);
+
+            redirect("/mod/".$usemodulename."/view.php?id=".$usemoduleid, '이 활동을 싫어합니다.', 2);
         } else {
             // update
-            $object_hate = new stdClass();
-            $object_hate->like = $datas->like;
-            $object_hate->hate = $datas->hate+1;
-            $object_hate->chance = 1;
-
-            $DB->update_record($datas, $object_hate);            
+            $object_like = new stdClass();
+            $object_like->userid = $userid;
+            $object_like->cid = $cid;
+            $object_like->cmid = $cmid;
+            $object_like->liked = 0;
+            $object_like->hate = 1;
+            $object_like->chance = 1;
+            
+            $DB->update_record('preference', $object_like);
+            redirect("/mod/".$usemodulename."/view.php?id=".$usemoduleid, '이 활동을 싫어합니다.', 2);           
         }
     }
 
-    function getChance($cid, $cmid){
+    function getChance($userid,$cid, $cmid){
         global $DB;
 
         $sql = "SELECT * 
                 FROM mdl_course c
                 JOIN mdl_course_modules cm ON cm.course = c.id
                 JOIN mdl_preference p ON p.cmid = cm.id
-                WHERE c.id = :cid AND cm.id = :cmid ";
+                WHERE c.id = :cid AND cm.id = :cmid AND p.userid = :userid ";
 
-        $datas = $DB->get_record_sql($sql, array('cid'=>$cid, 'cmid'=>$cmid));
+        $datas = $DB->get_record_sql($sql, array('cid'=>$cid, 'cmid'=>$cmid, 'userid'=>$userid));
 
         if(empty($datas->chance)){
             $chancedata = 0;
@@ -115,3 +137,19 @@
         return $chancedata;
     }
     
+    function getTotal($cid, $cmid){
+        global $DB;
+
+        $sql = "SELECT SUM(liked) total_like, SUM(hate) total_hate
+                FROM mdl_preference
+                WHERE cid = :cid AND cmid = :cmid";
+
+        $datas = $DB->get_record_sql($sql, array('cid'=>$cid, 'cmid'=>$cmid));
+                                                        
+        if($datas->total_like == 0){
+            $datas -> total_like = 0;
+            $datas -> total_hate = 0;
+        }
+        
+        return $datas;
+    }
